e7b7381c3230cfdaf1f53e5c5104282a9c9911f1
yuanqiuzhuang <qzyuan@njust.edu.cn>
2017-07-18 23:44:05 +0800

    network.py完善

--- src/network.py
+++ src/network.py
@@ -2,6 +2,11 @@
 __author__ = 'qzyuan'
 __date__ = '2017/7/18 22:38'
 
+"""
+一个实现前馈神经网络的sgd学习算法模块。使用反向传播计算梯度。
+"""
+
+import random
 import numpy as np
 
 class Network(object):
@@ -12,4 +17,13 @@ class Network(object):
         self.bias = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for (y, x) in zip(sizes[1:], sizes[:-1])]
+    
 
+### 函数定义
+def sigmoid(z):
+    """sigmoid函数。"""
+    return 1.0 / (1.0 + np.exp(-z))
+
+def sigmoid_prim(z):
+    """sigmoid函数的导数。"""
+    return sigmoid(z) * (1 - sigmoid(z))
